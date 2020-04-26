e44d592d7310ae1ccfc8dd1a116208da92cf3567
KiryaRizhiy <kilybox@gmail.com>
2020-04-26 15:23:17 +0300

    Done
    Done

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -6,7 +6,7 @@ public static class Settings
 {
     public static float rotationLimit = 3.14f;
     public static float accelerationLimit = 1f;
-    public static string googlePlayId = "3541210";
+    public static string googlePlayId = "3574547";
     public static string videoPlacementId = "video";
     public static bool testMode = false;
     //public static float brakingStages = 0.9f;
